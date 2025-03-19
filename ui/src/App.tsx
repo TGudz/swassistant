73c44338f7b238ebbaf61a3c9c36d6040b4569c1
Taras Gudz <taras.gudz@evolven.com>
2025-03-19 12:28:05 +0200

    add VebView setup; ui (react app); parcel setup

---
+++ ui/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-blue-600 to-purple-500 p-10">
+      <p className="text-white/80 text-xl font-semibold">
+      Taras 1
+      </p>
+    </div>
+  );
+}
+
+export default App;
